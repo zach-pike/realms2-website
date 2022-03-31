c851f03b2df0ea03ee5eb6397926880258c3d02f
ZachTurnPike <zachturnpike@gmail.com>
2022-03-30 22:35:43 -0400

    Grammar.

--- src/Pages/Home.svelte
+++ src/Pages/Home.svelte
@@ -12,7 +12,7 @@ import WhatsNewItem from "../PageComponets/WhatsNewList/WhatsNewItem.svelte";
         <WhatsNewItem>
             <p class="text-3xl bold">Website and server are in development!</p>
             <BetterHr />
-            <p>Our development team is working very hard to build the plugins for Realms to make it the best experience for the moderators and users, while that is happening, I am developing this website and plugins for the server</p>
+            <p>Our development team is working very hard to build the plugins for Realms to make it the best experience for the moderators and users, while that is happening, I am developing this website and plugins for the server.</p>
         </WhatsNewItem>
     </WhatsNew>
 </div>
