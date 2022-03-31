82b56105ffa347c54d2156dfacec41578382e673
ZachTurnPike <zachturnpike@gmail.com>
2022-03-31 17:00:53 -0400

    Testing cards

---
+++ src/PageComponets/Card/Card.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+
+</script>
+
+<div class="rounded-md p-4 bg-gray-300 w-fit shadow-md">
+    <slot></slot>
+</div>
---
+++ src/Pages/Contact.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+import Card from "../PageComponets/Card/Card.svelte";
+
+
+</script>
+
+<Card>
+    <p>Card test</p>
+</Card>
